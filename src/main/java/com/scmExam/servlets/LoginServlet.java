f6d4d2cd7d6c27563d737f7756d80437ebad30df
camilo4988 <ingcamilolopez2017@gmail.com>
2018-09-13 02:36:31 -0500

    commit

---
+++ src/main/java/com/scmExam/servlets/LoginServlet.java
@@ -0,0 +1,33 @@
+package com.scmExam.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet {
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String user = req.getParameter("user");
+		String pass = req.getParameter("password");
+		if ("scmExam".equals(user) && "eli4java".equals(pass)) {
+			response(resp, "Autenticacion correcta");
+		} else {
+			response(resp, "Autenticacion incorrecta, verifica tus credenciales e intentalo nuevamente");
+		}
+	}
+
+	private void response(HttpServletResponse resp, String msg)
+			throws IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<t1>" + msg + "</t1>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+}
